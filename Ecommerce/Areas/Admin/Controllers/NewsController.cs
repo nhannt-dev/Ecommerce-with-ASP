bddf57060ba46349a8058c7cddc8ba7e7558a9cd
nhannt112113 <nguyentrongnhan04072000@gmail.com>
2023-05-19 18:37:58 +0700

    add news with CKEDITOR & CKFINDER

---
+++ Ecommerce/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,43 @@
+﻿using Ecommerce.Models;
+using Ecommerce.Models.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce.Areas.Admin.Controllers
+{
+    public class NewsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/News
+        public ActionResult Index()
+        {
+            var items = db.News.OrderByDescending(x => x.Id).ToList();
+            return View(items);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(News model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                model.CategoryId = 3;
+                model.Alias = Ecommerce.Models.Common.Filter.FilterChar(model.Title);
+                db.News.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
